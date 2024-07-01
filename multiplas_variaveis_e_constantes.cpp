f9e56b09a345b1c173baf1faf6a0e48791608607
Lucas Yuji <160159812+LucaYuji@users.noreply.github.com>
2024-07-01 20:20:51 -0300

    Add files via upload

---
+++ multiplas_variaveis_e_constantes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define canal cout << "Canal 1";
+
+int main(){
+    //declaração multipla de variaveis
+    int vidas = 3, tiros = 500, life = 100;
+    #define oi cout << "Bom dia";
+    cout << pi << "\n";
+    canal
+    oi
+    cout << vidas << "\n" << tiros << "\n" << life << "\n";
+    return 0;
+}
